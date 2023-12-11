82f137d1edea1ea02eb8229a221d8cb330eba664
pruetj <james_pruet@oxy.com>
2023-12-11 17:20:46 -0600

    almost done

--- day10/main.go
+++ day10/main.go
@@ -3,333 +3,156 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	mazeLoop := Question1()
-	Question2(mazeLoop)
+	Question1()
+	//Question2()
 }
 
-type Maze []string
+type Universe struct {
+	Rows []string
+}
+
+// init universe
+var u Universe = Universe{}
 
 // Create a function to read the data
-func ReadData() ([]string, [2]int) {
+func ReadData() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 	s := bufio.NewScanner(file)
-	s_id := 0
-	maze := Maze{}
-	SPosition := [2]int{}
-	counter := 1
 	for s.Scan() {
-		// padding
-		if s_id == 0 {
-			maze = append(maze, strings.Repeat(".", len(s.Text())+2))
-			s_id++
-		}
-
-		// read the line
-		maze = append(maze, "."+s.Text()+".")
-
-		// Find index of "S" in the line
-		if i := strings.Index(s.Text(), "S"); i != -1 {
-			SPosition[0] = counter
-			SPosition[1] = i + 1
-
-		}
-
-		counter += 1
-
+		u.Rows = append(u.Rows, s.Text())
 	}
-
-	// padding
-	maze = append(maze, strings.Repeat(".", len(maze[0])))
-
-	return maze, SPosition
 }
 
-func Question1() [][]int {
-	maze, StartPosition := ReadData()
-	origStartRow, origStartCol := StartPosition[0], StartPosition[1]
+func Question1() {
+	ReadData()
+	u.Expand(2, 2)
+	u.FindAndLabel()
+	u.Distance()
 
-	startRow, StartCol := StartPosition[0], StartPosition[1]
-	mazeLoop := [][]int{}
-	var prevRow, prevCol int
+}
 
-	for {
-		//fmt.Println("SR", startRow, "SC", StartCol, "PR", prevRow, "PC", prevCol)
+func (u *Universe) Distance() {
+	// Find manhattan distance between each #
+	// add up distance for each pair
+	// pairs to only be counted once
 
-		if string(maze[startRow][StartCol]) == "S" {
-			switch {
-			case string(maze[startRow-1][StartCol]) == "|" || string(maze[startRow-1][StartCol]) == "F" || string(maze[startRow-1][StartCol]) == "7":
-				prevRow = startRow
-				prevCol = StartCol
-				startRow--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case string(maze[startRow+1][StartCol]) == "|" || string(maze[startRow][StartCol+1]) == "L" || string(maze[startRow][StartCol+1]) == "J":
-				prevRow = startRow
-				prevCol = StartCol
-				startRow++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case string(maze[startRow][StartCol-1]) == "-" || string(maze[startRow][StartCol-1]) == "L" || string(maze[startRow][StartCol-1]) == "F":
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case string(maze[startRow][StartCol+1]) == "-" || string(maze[startRow][StartCol+1]) == "J" || string(maze[startRow][StartCol+1]) == "7":
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "|" {
-			switch {
-			case (string(maze[startRow-1][StartCol]) == "|" || string(maze[startRow-1][StartCol]) == "7" || string(maze[startRow-1][StartCol]) == "F" || string(maze[startRow-1][StartCol]) == "S") && prevRow != startRow-1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow+1][StartCol]) == "|" || string(maze[startRow+1][StartCol]) == "L" || string(maze[startRow+1][StartCol]) == "J" || string(maze[startRow+1][StartCol]) == "S") && prevRow != startRow+1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "-" {
-			switch {
-			case (string(maze[startRow][StartCol-1]) == "-" || string(maze[startRow][StartCol-1]) == "L" || string(maze[startRow][StartCol-1]) == "F" || string(maze[startRow][StartCol-1]) == "S") && prevCol != StartCol-1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow][StartCol+1]) == "-" || string(maze[startRow][StartCol+1]) == "J" || string(maze[startRow][StartCol+1]) == "7" || string(maze[startRow][StartCol+1]) == "S") && prevCol != StartCol+1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "L" {
-			switch {
-			case (string(maze[startRow-1][StartCol]) == "|" || string(maze[startRow-1][StartCol]) == "F" || string(maze[startRow-1][StartCol]) == "7" || string(maze[startRow-1][StartCol]) == "S") && prevRow != startRow-1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow][StartCol+1]) == "-" || string(maze[startRow][StartCol+1]) == "J" || string(maze[startRow][StartCol+1]) == "7" || string(maze[startRow][StartCol+1]) == "S") && prevCol != StartCol+1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "J" {
-			switch {
-			case (string(maze[startRow-1][StartCol]) == "|" || string(maze[startRow-1][StartCol]) == "F" || string(maze[startRow-1][StartCol]) == "7" || string(maze[startRow-1][StartCol]) == "S") && prevRow != startRow-1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow][StartCol-1]) == "-" || string(maze[startRow][StartCol-1]) == "L" || string(maze[startRow][StartCol-1]) == "F" || string(maze[startRow][StartCol-1]) == "S") && prevCol != StartCol-1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "7" {
-			switch {
-			case (string(maze[startRow+1][StartCol]) == "|" || string(maze[startRow+1][StartCol]) == "L" || string(maze[startRow+1][StartCol]) == "J" || string(maze[startRow+1][StartCol]) == "S") && prevRow != startRow+1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow][StartCol-1]) == "-" || string(maze[startRow][StartCol-1]) == "L" || string(maze[startRow][StartCol-1]) == "F" || string(maze[startRow][StartCol-1]) == "S") && prevCol != StartCol-1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol--
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			}
-		} else if string(maze[startRow][StartCol]) == "F" {
-			switch {
-			case (string(maze[startRow][StartCol+1]) == "-" || string(maze[startRow][StartCol+1]) == "7" || string(maze[startRow][StartCol+1]) == "J" || string(maze[startRow][StartCol+1]) == "S") && prevCol != StartCol+1:
-				prevRow = startRow
-				prevCol = StartCol
-				StartCol++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
-			case (string(maze[startRow+1][StartCol]) == "|" || string(maze[startRow+1][StartCol]) == "J" || string(maze[startRow+1][StartCol]) == "L" || string(maze[startRow+1][StartCol]) == "S") && prevRow != startRow+1:
-				prevRow = startRow
-				prevCol = StartCol
-				startRow++
-				mazeLoop = append(mazeLoop, []int{startRow, StartCol})
+	for _, v := range u.Rows {
+		fmt.Println(v)
+	}
+
+	distance := 0
+	for row, v := range u.Rows {
+		for col := range v {
+			if u.Rows[row][col] != '.' {
+				// Find all other #'s
+				for row2, v2 := range u.Rows {
+					for col2, _ := range v2 {
+						val1, _ := strconv.Atoi(string(u.Rows[row][col]))
+						val2, _ := strconv.Atoi(string(u.Rows[row2][col2]))
+						if u.Rows[row2][col2] != '.' && val1 < val2 {
+							// Calculate manhattan distance
+							// Add to total
+							distance += int(math.Abs(manhattanDistance(row, col, row2, col2)))
+							fmt.Println("Distance between", string(u.Rows[row][col]), "and", string(u.Rows[row2][col2]), "is", manhattanDistance(row, col, row2, col2))
+						}
+					}
+				}
 			}
 		}
+	}
 
-		if startRow == origStartRow && StartCol == origStartCol {
-			break
+	fmt.Println("Q1:", distance)
+}
+
+func manhattanDistance(row1, col1, row2, col2 int) float64 {
+	return float64(math.Abs((float64(row1) - float64(row2))) + math.Abs((float64(col1) - float64(col2))))
+}
+
+func (u *Universe) FindAndLabel() {
+	// Find each # in the map and label it with a number counting upwards from the first
+	counter := 1
+	for row, v := range u.Rows {
+		for col, _ := range v {
+			if u.Rows[row][col] == '#' {
+				u.Rows[row] = u.Rows[row][:col] + strconv.Itoa(counter) + u.Rows[row][col+1:]
+				counter++
+			}
+		}
+	}
+}
+
+func (u *Universe) Expand(rowExp, colExp int) {
+	expandedU := Universe{}
+	colExp--
+
+	// Expand rows
+	for _, v := range u.Rows {
+		if isAllSameCharacter(v) {
+			for i := 0; i < rowExp; i++ {
+				expandedU.Rows = append(expandedU.Rows, v)
+			}
+		} else {
+			expandedU.Rows = append(expandedU.Rows, v)
+		}
+	}
+
+	// Search each column for same character
+	// If the characters are all the same, expand the column by colExp
+
+	fmt.Println("Col expansion")
+	totalLoops := len(expandedU.Rows[0])
+	currentCol := 0
+	for col := 0; col < totalLoops; col++ {
+		sameCharCheck := ""
+		for _, r := range u.Rows {
+			sameCharCheck += string(r[col])
+		}
+
+		expandedURowsandCols := Universe{}
+		if isAllSameCharacter(sameCharCheck) {
+			fmt.Println(currentCol)
+			for rowE, _ := range expandedU.Rows {
+				newRow := expandedU.Rows[rowE][:(currentCol)] + strings.Repeat(".", colExp) + expandedU.Rows[rowE][(currentCol):]
+				expandedURowsandCols.Rows = append(expandedURowsandCols.Rows, newRow)
+			}
+			expandedU.Rows = expandedURowsandCols.Rows
+		}
+
+		if isAllSameCharacter(sameCharCheck) {
+			currentCol += 2
+		} else {
+			currentCol++
 		}
 
 	}
-	fmt.Println("Q1: ", len(mazeLoop)/2)
-	return mazeLoop
 
-	// fmt.Println("Q1: ", answer)
+	u.Rows = expandedU.Rows
+}
 
+func isAllSameCharacter(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+
+	firstChar := s[0]
+
+	for i := 1; i < len(s); i++ {
+		if s[i] != firstChar {
+			return false
+		}
+	}
+
+	return true
 }
 
 func Question2(mazeLoop [][]int) {
-	// Rebuild maze usiing solved maze
-	// When LJ or JL is found, replace both with -. Same for F7 and 7F
-	// When L7 or 7L is found, replace one with | and the other with -. Same for FJ and JF
-	// So first, take solved maze and ignore "-", remove those from the solved maze solution
-	// Then go through maze and look for the L7 or 7L and  FJ and JF. Replace one with | and the other with -
-	// Finally, need ot understand what shape S is. Just manuualy do it
-	maze, _ := ReadData()
-	for _, v := range maze {
-		fmt.Println(v)
-	}
 
-	MazeLoopNoDash := [][]int{}
-
-	// remove "-" from mazeLoop
-	for _, v := range mazeLoop {
-		if string(maze[v[0]][v[1]]) != "-" {
-			MazeLoopNoDash = append(MazeLoopNoDash, v)
-		}
-	}
-
-	// Replace 7F, F7, JL, LJ, SJ, JS
-	for i := range MazeLoopNoDash {
-		if i == len(MazeLoopNoDash)-1 {
-			break
-		}
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "7" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "F" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "F" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "7" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "J" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "L" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "J" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "S" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "L" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "J" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "S" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "J" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "-" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-	}
-
-	// Replace L7, 7L, FJ, JF, S7, 7S
-	for i := range MazeLoopNoDash {
-		if i == len(MazeLoopNoDash)-1 {
-			break
-		}
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "L" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "7" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "7" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "L" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "S" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "7" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "7" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "S" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "F" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "J" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-		if string(maze[MazeLoopNoDash[i][0]][MazeLoopNoDash[i][1]]) == "J" {
-			if string(maze[MazeLoopNoDash[i+1][0]][MazeLoopNoDash[i+1][1]]) == "F" {
-				maze[MazeLoopNoDash[i][0]] = maze[MazeLoopNoDash[i][0]][:MazeLoopNoDash[i][1]] + "|" + maze[MazeLoopNoDash[i][0]][(MazeLoopNoDash[i][1]+1):]
-				maze[MazeLoopNoDash[i+1][0]] = maze[MazeLoopNoDash[i+1][0]][:MazeLoopNoDash[i+1][1]] + "-" + maze[MazeLoopNoDash[i+1][0]][(MazeLoopNoDash[i+1][1]+1):]
-			}
-		}
-	}
-
-	for _, v := range maze {
-		fmt.Println(v)
-	}
-
-	Solver2(maze, mazeLoop)
-
-}
-
-// 544 too
-func Solver2(maze []string, solvedPuzzle [][]int) int {
-	// loop through maze row by row, left to right
-	// Check if character is part of solved puzzle. If so, don't count
-	// Otherwise, reading left to right, a point with an odd number of "|" on the left side counts
-	// A point with an even number of || on the left side does not count
-
-	total := 0
-	for row := range maze {
-		fmt.Println(maze[row])
-		left := 0
-	loop:
-		for col := range maze[row] {
-
-			for _, v := range solvedPuzzle {
-				if row == v[0] && col == v[1] {
-					if string(maze[row][col]) == "|" {
-						left++
-					}
-					continue loop
-				}
-			}
-
-			if left%2 != 0 {
-				fmt.Println(row, col, left, string(maze[row][col]))
-				total++
-			}
-		}
-	}
-
-	fmt.Println(total)
-	return 0
 }
