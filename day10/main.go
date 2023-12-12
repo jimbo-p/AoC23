12974440e798517bd1e8aaba9224ae2b5cb913c4
Andrew Pruet <james_pruet@oxy.com>
2023-12-11 22:34:54 -0600

    disaster.. also replaced day 10 with day 11 :/

--- day10/main.go
+++ day10/main.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
-	Question1()
-	//Question2()
+	// Question1()
+	Question2()
 }
 
 type Universe struct {
@@ -34,34 +33,130 @@ func ReadData() {
 func Question1() {
 	ReadData()
 	u.Expand(2, 2)
-	u.FindAndLabel()
-	u.Distance()
-
+	intVerse := u.FindAndLabel()
+	u.Distance(intVerse)
 }
 
-func (u *Universe) Distance() {
+// 560822911938
+// 82000210 too low
+func Question2() {
+	u = Universe{}
+	ReadData()
+	m := u.Expand2(1000000-1, 1000000-1)
+
+	points := getKeys(m)
+	keyCombs := keyCombinations(points)
+
+	total := 0
+	for _, kc := range keyCombs {
+		// Get the values corresponding to each combination of keys
+		val1, ok1 := m[kc[0]]
+		val2, ok2 := m[kc[1]]
+		if ok1 && ok2 {
+			total += manhattanDistance2(val1.row, val1.col, val2.row, val2.col)
+		}
+	}
+
+	fmt.Println(total)
+}
+
+type Point struct {
+	row int
+	col int
+}
+
+func (u *Universe) Expand2(rowExp, colExp int) map[Point]Point {
+	// map of old point and new point
+	expMap := make(map[Point]Point)
+	for row, v := range u.Rows {
+		for col, _ := range v {
+			if u.Rows[row][col] == '#' {
+				expMap[Point{row, col}] = Point{row, col}
+			}
+		}
+	}
+
+	fmt.Println(expMap)
+
+	// Expand cols
+	for row, v := range u.Rows {
+		if isAllSameCharacter(v) {
+			for k, val := range expMap {
+				if k.row > row {
+					expMap[k] = Point{val.row + rowExp, val.col}
+				}
+			}
+		}
+	}
+
+	fmt.Println(expMap)
+
+	// expand rows
+	for col := 0; col < len(u.Rows[0]); col++ {
+		sameCharCheck := ""
+		for _, r := range u.Rows {
+			sameCharCheck += string(r[col])
+		}
+
+		if isAllSameCharacter(sameCharCheck) {
+			for k, v := range expMap {
+				if k.col > col {
+					expMap[k] = Point{v.row, v.col + colExp}
+				}
+			}
+		}
+	}
+
+	fmt.Println(expMap)
+	return expMap
+}
+
+// Function to collect keys from the map
+func getKeys(m map[Point]Point) []Point {
+	var keys []Point
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Function to generate unique combinations of keys
+func keyCombinations(keys []Point) [][2]Point {
+	var combs [][2]Point
+
+	for i := 0; i < len(keys); i++ {
+		for j := i + 1; j < len(keys); j++ {
+			combs = append(combs, [2]Point{keys[i], keys[j]})
+		}
+	}
+
+	return combs
+}
+
+func manhattanDistance2(row1, col1, row2, col2 int) int {
+	return int(math.Abs((float64(row1) - float64(row2))) + math.Abs((float64(col1) - float64(col2))))
+}
+
+func (u *Universe) Distance(matrix [][]int16) {
 	// Find manhattan distance between each #
 	// add up distance for each pair
 	// pairs to only be counted once
 
-	for _, v := range u.Rows {
-		fmt.Println(v)
-	}
-
 	distance := 0
-	for row, v := range u.Rows {
+	for row, v := range matrix {
+		fmt.Println("Completed row: ", row)
 		for col := range v {
-			if u.Rows[row][col] != '.' {
+			if matrix[row][col] != 0 {
 				// Find all other #'s
-				for row2, v2 := range u.Rows {
-					for col2, _ := range v2 {
-						val1, _ := strconv.Atoi(string(u.Rows[row][col]))
-						val2, _ := strconv.Atoi(string(u.Rows[row2][col2]))
-						if u.Rows[row2][col2] != '.' && val1 < val2 {
+				for row2, v2 := range matrix {
+					for col2 := range v2 {
+						val1 := matrix[row][col]
+						val2 := matrix[row2][col2]
+						if matrix[row2][col2] != 0 && val1 < val2 {
 							// Calculate manhattan distance
 							// Add to total
-							distance += int(math.Abs(manhattanDistance(row, col, row2, col2)))
-							fmt.Println("Distance between", string(u.Rows[row][col]), "and", string(u.Rows[row2][col2]), "is", manhattanDistance(row, col, row2, col2))
+							distance += int(math.Abs(manhattanDistance(int16(row), int16(col), int16(row2), int16(col2))))
+							// fmt.Println("Distance between", matrix[row][col], "and", matrix[row2][col2], "is", manhattanDistance(row, col, row2, col2))
 						}
 					}
 				}
@@ -72,21 +167,31 @@ func (u *Universe) Distance() {
 	fmt.Println("Q1:", distance)
 }
 
-func manhattanDistance(row1, col1, row2, col2 int) float64 {
+func manhattanDistance(row1, col1, row2, col2 int16) float64 {
 	return float64(math.Abs((float64(row1) - float64(row2))) + math.Abs((float64(col1) - float64(col2))))
 }
 
-func (u *Universe) FindAndLabel() {
+func (u *Universe) FindAndLabel() [][]int16 {
 	// Find each # in the map and label it with a number counting upwards from the first
-	counter := 1
+	matrix := make([][]int16, len(u.Rows))
+
+	for i := range matrix {
+		// Initialize each sub-slice (row) with 10 ints, all set to 0
+		matrix[i] = make([]int16, len(u.Rows[i]))
+	}
+
+	var counter int16 = 1
 	for row, v := range u.Rows {
 		for col, _ := range v {
 			if u.Rows[row][col] == '#' {
-				u.Rows[row] = u.Rows[row][:col] + strconv.Itoa(counter) + u.Rows[row][col+1:]
+				matrix[row][col] = counter
 				counter++
+			} else {
+				matrix[row][col] = 0
 			}
 		}
 	}
+	return matrix
 }
 
 func (u *Universe) Expand(rowExp, colExp int) {
@@ -151,8 +256,4 @@ func isAllSameCharacter(s string) bool {
 	}
 
 	return true
-}
-
-func Question2(mazeLoop [][]int) {
-
 }
